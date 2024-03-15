6028814cbecdbb4e62871e7bb2df85f023ca07df
Ido Pesok <idopesok91@gmail.com>
2024-03-14 18:35:32 -0700

    deleted old code

--- agents/agent_uci.py
+++
@@ -1,1 +0,0 @@
-from gym_env.enums import Action
--- gym_env/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Registration to the gym"""
-from gym.envs.registration import register
-
-register(id='neuron_poker-v0',
-         entry_point='gym_env.env:HoldemTable')
